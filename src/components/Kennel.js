55ad64b0464bd817fa9d6fa44913d43ed6550977
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-05-20 08:24:01 -0500

    added locations to kennel

---
+++ src/components/Kennel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
+import LocationList from "./LocationList/LocationList"
+
+
+export default class Kennel extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Student Kennels</h3>
+                <LocationList />
+                <EmployeeList />
+            </div>
+        );
+    }
+}
---
+++ src/components/LocationList/LocationList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Location extends Component {
+    render() {
+        return (
+            <section>
+                <h4>Nashville North</h4>
+                <h5>416 Sunnyside Lane N. Nashville, TN</h5>
+                <h4>Nashville South</h4>
+                <h5>316 Stonecold Ave. SW Nashville, TN</h5>
+            </section>
+        )
+    }
+}
